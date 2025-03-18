d04451a01c553f7a453eb998255e32bc842b1449
Cong Nguyen <72590623+EmbeddedOS@users.noreply.github.com>
2025-03-18 16:54:24 +0700

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -66,7 +66,7 @@ make -j$(nproc) ARCH=arm64 CROSS_COMPILE=../toolchain/bin/aarch64-none-linux-gnu
 
 # 6. Build TF-A
 git clone https://github.com/ARM-software/arm-trusted-firmware.git
-make CROSS_COMPILE=../toolchain/bin/aarch64-none-linux-gnu- PLAT=qemu all fip DEBUG=1 BL33=u-boot/u-boot.bin -j$(nproc) -c arm-trusted-firmware
+make CROSS_COMPILE=../toolchain/bin/aarch64-none-linux-gnu- PLAT=qemu all fip DEBUG=1 BL33=../u-boot/u-boot.bin -j$(nproc) -C arm-trusted-firmware
 dd if=arm-trusted-firmware/build/qemu/debug/bl1.bin of=flash.bin bs=4096 conv=notrunc
 dd if=arm-trusted-firmware/build/qemu/debug/fip.bin of=flash.bin seek=64 bs=4096 conv=notrunc
 
